67860296ba5cbab7738c0a4acb623342158eb8a2
Tushar Gangurde <tushar.gangurde92@gmail.com>
2024-08-12 20:51:15 +0530

    linked list study & code completed

---
+++ src/datastructure/linkedlist/Main.java
@@ -0,0 +1,79 @@
+package datastructure.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        //show Head, Tail & length
+//        LinkedList myLinkedList = new LinkedList(4);
+//        myLinkedList.getHeadNode();
+//        myLinkedList.getTailNode();
+//        myLinkedList.getTotalLength();
+
+        //append node at the end
+//        LinkedList myLinkedList = new LinkedList(4);
+//        myLinkedList.append(2);
+//        myLinkedList.printList();
+
+        //remove last node
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast());
+
+
+        //prepend node
+//        LinkedList myLinkedList = new LinkedList(2);
+//        myLinkedList.append(3);
+//        myLinkedList.prepend(1);
+//        myLinkedList.printList();
+
+        //remove first
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        System.out.println(myLinkedList.removeFirst().value);
+//        System.out.println(myLinkedList.removeFirst().value);
+//        System.out.println(myLinkedList.removeFirst().value);
+//        System.out.println(myLinkedList.removeFirst().value);
+
+        //Get node
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.append(4);
+//        System.out.println(myLinkedList.get(2).value);
+
+
+        //Set node
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.append(4);
+//        myLinkedList.set(2, 5);
+//        myLinkedList.printList();
+
+        //insert node
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.append(4);
+//        myLinkedList.insert(2, 11);
+//        myLinkedList.printList();
+
+        //remove node
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.append(4);
+//        myLinkedList.remove(1);
+//        myLinkedList.printList();
+
+        //reverse linked list
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//        myLinkedList.append(4);
+//        myLinkedList.reverse();
+//        myLinkedList.printList();
+    }
+}
